e88e6c711f9b5008e520cb8d06896808178bab7f
rahul prakash <rahulprakash11@gmail.com>
2022-11-14 17:26:51 +0530

    social

--- src/components/home/social/data.tsx
+++ src/components/home/social/data.tsx
@@ -1,10 +1,36 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 
+// export const socialList = {
+//     git : "https://github.com/RahulPrakash11",
+//     linkedIn : "https://www.linkedin.com/in/rahul-prakash-81b30821",
+//     twitter : "https://twitter.com/rahulprakash111/",
+//     instagram : "https://github.com/RahulPrakash11",
+// }
 
-export const socialList = {
-    git : "https://github.com/RahulPrakash11",
-    linkedIn : "https://www.linkedin.com/in/rahul-prakash-81b30821",
-    twitter : "https://twitter.com/rahulprakash111/",
-    instagram : "https://github.com/RahulPrakash11",
-}
 
+export const socialList = [
+    {
+        id: 1,
+        link: "https://github.com/RahulPrakash11",
+        icon: GitHubIcon
+    },
+    {
+        id: 2,
+        link: "https://www.linkedin.com/in/rahul-prakash-81b30821",
+        icon: LinkedInIcon
+    },
+    {
+        id: 3,
+        link: "https://twitter.com/rahulprakash111/",
+        icon: TwitterIcon
+    },
+    // {
+    //     id: 4,
+    //     link: "https://github.com/RahulPrakash11",
+    //     icon: InstagramIcon
+    // }
+]
--- src/components/home/social/social.tsx
+++ src/components/home/social/social.tsx
@@ -3,10 +3,6 @@ import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import { ThemeProvider } from "styled-components";
 import theme from "../../../hooks/themes";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
 import Link from "@mui/material/Link";
 import Divider from "@mui/material/Divider";
 import { socialList } from "./data";
@@ -26,38 +22,16 @@ const MySocial = () => {
                 <Box sx={{ display: { xs: "none", md: "flex" } , flexGrow: {xs: 'none', md: 1}, height: "6rem", marginTop: "2rem" }} justifyContent="center">
                     <Divider color="#a8b2d1" orientation="vertical" sx={{ mx: 1 }}/>
                 </Box>
-                <Link href="#" onClick={() => window.open(`${socialList.git}`, '_blank', 'noopener,noreferrer')}>
-                    <Box sx={{ display: "flex", justifyContent: "center", paddingY:"8px", paddingX : { xs: "8px", md : "8px" }, color: "#b98a6f", "&:hover": {paddingTop: "0px", paddingBottom: "16px", color: "#51bda3", cursor: "pointer",}}}>
-                        <IconButton color="inherit" sx={{ padding: "0px" }}>
-                            {/* {socialList.icon} */}
-                            <GitHubIcon fontSize="large"/>
-                        </IconButton>
-                    </Box>
-                </Link>
-                <Link href="#" onClick={() => window.open(`${socialList.linkedIn}`, '_blank', 'noopener,noreferrer')}>
-                    <Box sx={{ display: "flex", justifyContent: "center", paddingY:"8px", paddingX : { xs: "8px", md : "8px" }, color: "#b98a6f", "&:hover": {paddingTop: "0px", paddingBottom: "16px", color: "#51bda3", cursor: "pointer",}}}>
-                        <IconButton color="inherit" sx={{ padding: "0px" }}>
-                            {/* {socialList.icon} */}
-                            <LinkedInIcon fontSize="large"/>
-                        </IconButton>
-                    </Box>
-                </Link>
-                <Link href="#" onClick={() => window.open(`${socialList.twitter}`, '_blank', 'noopener,noreferrer')}>
-                    <Box sx={{ display: "flex", justifyContent: "center", paddingY:"8px", paddingX : { xs: "8px", md : "8px" }, color: "#b98a6f", "&:hover": {paddingTop: "0px", paddingBottom: "16px", color: "#51bda3", cursor: "pointer",}}}>
-                        <IconButton color="inherit" sx={{ padding: "0px" }}>
-                            {/* {socialList.icon} */}
-                            <TwitterIcon fontSize="large"/>
-                        </IconButton>
-                    </Box>
-                </Link>
-                <Link href="#" onClick={() => window.open(`${socialList.instagram}`, '_blank', 'noopener,noreferrer')}>
-                    <Box sx={{ display: "flex", justifyContent: "center", paddingY:"8px", paddingX : { xs: "8px", md : "8px" }, color: "#b98a6f", "&:hover": {paddingTop: "0px", paddingBottom: "16px", color: "#51bda3", cursor: "pointer",}}}>
-                        <IconButton color="inherit" sx={{ padding: "0px" }}>
-                            {/* {socialList.icon} */}
-                            <InstagramIcon fontSize="large"/>
-                        </IconButton>
-                    </Box>
-                </Link>
+                { socialList.map((item) => (
+                    <Link href="#" onClick={() => window.open(`${item.link}`, '_blank', 'noopener,noreferrer')}>
+                        <Box sx={{ display: "flex", justifyContent: "center", paddingY:"8px", paddingX : { xs: "8px", md : "8px" }, color: "#b98a6f", "&:hover": {paddingTop: "0px", paddingBottom: "16px", color: "#51bda3", cursor: "pointer",}}}>
+                            <IconButton color="inherit" sx={{ padding: "0px" }}>
+                                {/* {socialList.icon} */}
+                                <item.icon fontSize="large"/>
+                            </IconButton>
+                        </Box>
+                    </Link>
+                ))}
             </Box>
         </ThemeProvider>
     )
