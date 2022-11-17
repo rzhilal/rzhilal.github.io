9953785380e64a7c81d0bdfe440f31c3bd2a29e4
rahul prakash <rahulprakash11@gmail.com>
2022-11-17 17:19:03 +0530

    resume update

--- src/constants/data.tsx
+++ src/constants/data.tsx
@@ -1,10 +1,5 @@
 
-// const resumeLink = "https://drive.google.com/uc?id=1t-D_9VueacqpYDorhO9RAcd0BZjbFZTB&export=download"
 
-// https://docs.google.com/document/d/1FXOXh4DO-ryfgLjk7FPwZevPI2pQFxlF8WgnmU5KeFk/edit?usp=sharing
-// https://drive.google.com/uc?id=1t-D_9VueacqpYDorhO9RAcd0BZjbFZTB&export=download
-// https://drive.google.com/file/d/1t-D_9VueacqpYDorhO9RAcd0BZjbFZTB/view?usp=share_link
-// https://drive.google.com/file/d/1XZ3HWaFJL9tiKyRvLmk4ct_JZjeHG6JT/view?usp=sharing
 
-export const resumeLink = "https://drive.google.com/uc?id=1XZ3HWaFJL9tiKyRvLmk4ct_JZjeHG6JT&export=download"
+export const resumeLink = "https://drive.google.com/uc?id=1QsntP1X3Nu8Zht6jk7JeLsuiQ2QMO_51&export=download"
 export const email = "rahulprakash11@gmail.com"
